6650a9bbfd4a2e50d5eda20576638da1f3892489
Zen <dwipjana293@gmail.com>
2021-10-19 05:44:27 +0530

    new features

--- src/cogs/Activity.py
+++ src/cogs/Activity.py
@@ -13,7 +13,7 @@ class ActivityHandler(commands.Cog):
         await self.bot.wait_until_ready()
         activity = discord.Activity(
             type=discord.ActivityType.listening,
-            name=f'YouTube Music'
+            name=f'.help | .settings'
         )
         await self.bot.change_presence(
             status=discord.Status.online,
