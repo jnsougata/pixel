bf3550281e288549fefe9064072440b8648ee0ca
Zen <jnsougata@gmail.ccom>
2021-12-16 14:54:59 +0530

    optimization

--- src/views/subStreamer.py
+++ src/views/subStreamer.py
@@ -138,7 +138,7 @@ async def sub_view_streamer(
         interaction: discord.Interaction,
         bot: discord.Client
 ):
-    await ctx.send("` NOT FULLY IMPLEMENTED YET `")
+    await ctx.send(f"{Emo.WARN} **`NOT FULLY IMPLEMENTED YET`** {Emo.WARN}")
     data = await db_fetch_object(
         guildId=ctx.guild.id,
         key='streamer'
