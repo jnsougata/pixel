e97163a8369f6f3017db71f4971ff0142d3db460
Zen <jnsougata@gmail.com>
2022-02-26 17:07:36 +0530

    improvement

--- bot/cogs/listener.py
+++ bot/cogs/listener.py
@@ -83,7 +83,7 @@ class Listeners(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
         if not member.bot:
-
+            guild_id = member.guild.id
             raw = await db_fetch_object(guild_id=guild_id, key='welcome')
             if raw and raw[0].isdigit():
                 reception = member.guild.get_channel(int(raw[0]))
