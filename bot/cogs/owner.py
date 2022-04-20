a092df786e1c2681f55e33a766f335dd167c1735
Zen <jnsougata@gmail.com>
2022-04-20 10:52:43 +0530

    implemented caching

--- bot/cogs/owner.py
+++ bot/cogs/owner.py
@@ -3,8 +3,7 @@ import app_util
 import traceback
 from typing import Any
 from bot.extras.emojis import Emo
-from bot.extras.func import drive
-
+from bot.extras.func import drive, db_fetch_object
 import os
 import aiohttp
 from asyncdeta import Deta, Field, Update
@@ -51,27 +50,69 @@ class Override(app_util.Cog):
         command=app_util.SlashCommand(
             name='sync',
             description='syncs base to base', default_access=False,
+            options=[
+                app_util.IntOption(
+                    name='option',
+                    description='option to sync',
+                    required=True,
+                    choices=[
+                        app_util.Choice(name='channels', value=1),
+                        app_util.Choice(name='customs', value=2),
+                        app_util.Choice(name='receptions', value=3),
+                        app_util.Choice(name='receivers', value=4),
+                        app_util.Choice(name='ping_roles', value=5),
+                    ],
+                ),
+            ],
             overwrites=[app_util.Overwrite.for_user(516649677001719819)]
         ),
         guild_id=877399405056102431
     )
-    async def sync_command(self, ctx: app_util.Context):
+    async def sync_command(self, ctx: app_util.Context, option: int):
         await ctx.send_response('Check terminal. Sync in progress...')
-        session = aiohttp.ClientSession()
-        deta = Deta(project_key=os.getenv('DETA_TOKEN'), session=session)
-        keys = [str(guild.id) for guild in self.bot.guilds]
-        base = deta.base(f'01PIXEL')
-        for key in keys:
-            try:
-                await base.remove_field(key=key, field_name='ABC')
-            except Exception:
-                pass
-            try:
-                await base.remove_field(key=key, field_name='YOUTUBE')
-            except Exception:
-                pass
-        await session.close()
-        await ctx.send_followup('Sync complete.')
+
+        if option == 1:
+            for guild in self.bot.guilds:
+                old_channels = await db_fetch_object(guild_id=guild.id, key='youtube')
+                old_receivers = await db_fetch_object(guild_id=guild.id, key='receivers')
+                if old_channels and old_receivers:
+                    for channel in old_channels.keys():
+                        receiver = old_receivers.get(channel)
+                        old_channels[channel]['receiver'] = receiver
+                    await self.bot.db.add_field(key=str(guild.id), field=Field('CHANNELS', old_channels), force=True)
+                    print(old_channels)
+            await ctx.send_followup('[CHANNELS] Sync complete...')
+
+        if option == 2:
+            for guild in self.bot.guilds:
+                old_texts = await db_fetch_object(guild_id=guild.id, key='text') or {}
+                await self.bot.db.add_field(key=str(guild.id), field=Field('CUSTOM', old_texts), force=True)
+                print(old_texts)
+            await ctx.send_followup('[CUSTOM] Sync complete...')
+
+        if option == 3:
+            for guild in self.bot.guilds:
+                old_reception = await db_fetch_object(guild_id=guild.id, key='welcome')
+                data = old_reception[0] if old_reception and old_reception[0].isdigit() else None
+                await self.bot.db.add_field(key=str(guild.id), field=Field('RECEPTION', data), force=True)
+                print(old_reception)
+            await ctx.send_followup('[RECEPTION] Sync complete...')
+
+        if option == 4:
+            for guild in self.bot.guilds:
+                old_receiver = await db_fetch_object(guild_id=guild.id, key='alertchannel')
+                data = old_receiver[0] if old_receiver and old_receiver[0].isdigit() else None
+                await self.bot.db.add_field(key=str(guild.id), field=Field('RECEIVER', data), force=True)
+                print(old_receiver)
+            await ctx.send_followup('[RECEIVER] Sync complete...')
+
+        if option == 5:
+            for guild in self.bot.guilds:
+                old_ping_role = await db_fetch_object(guild_id=guild.id, key='arole')
+                data = old_ping_role[0] if old_ping_role and old_ping_role[0].isdigit() else None
+                await self.bot.db.add_field(key=str(guild.id), field=Field('PINGROLE', data), force=True)
+                print(old_ping_role)
+            await ctx.send_followup('[PINGROLE] Sync complete...')
 
 
 async def setup(bot: app_util.Bot):
